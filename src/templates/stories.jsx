fc3f32841f1b8c4780b2fdbf9b38b5842ac5f842
Rodrigo Leon <rodrigo@tectonica.co>
2024-09-23 10:43:18 -0300

    QA Fixes

--- src/templates/stories.jsx
+++ src/templates/stories.jsx
@@ -2,15 +2,22 @@ import * as React from 'react';
 import { graphql } from 'gatsby';
 import SeoDatoCMS from '../ui/components/seo-datocms';
 import ListWrapper from '../ui/layout/list-wrapper/list-wrapper';
+import StructuredTextDefault from '../ui/components/structured-text-default';
+import { CustomImage, Tags } from 'tectonica-ui';
 
 const StoryImpactPage = ({ data: { page, favicon } }) => {
-  const { title, seo } = page;
+  const { title, image, content, tags, seo } = page;
 
   return (
     <>
       <SeoDatoCMS seo={seo} favicon={favicon} />
-      <ListWrapper variant="lavander">
+      <ListWrapper variant="lavander stories">
         <h1>{title}</h1>
+        <div className="basic-content">
+          <CustomImage image={image} />
+          <StructuredTextDefault content={content} />
+          <Tags tags={tags} />
+        </div>
       </ListWrapper>
     </>
   );
@@ -27,6 +34,18 @@ export const StoryImpactPageQuery = graphql`
     }
     page: datoCmsStoriesImpact(id: { eq: $id }) {
       title
+      image {
+        width
+        height
+        alt
+        url
+      }
+      content {
+        value
+      }
+      tags {
+        ...Tag
+      }
       seo: seoMetaTags {
         ...GatsbyDatoCmsSeoMetaTags
       }
