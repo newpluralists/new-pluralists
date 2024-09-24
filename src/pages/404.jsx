0b6917d932e6239757c5159c1eef890898c73b84
Rodrigo Leon <rodrigo@tectonica.co>
2024-09-24 07:43:25 -0300

    Not found page added

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,35 +1,37 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { graphql, Link } from 'gatsby';
+import SeoDatoCMS from '../ui/components/seo-datocms';
 
-const pageStyles = {
-  color: '#232129',
-  padding: '96px',
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-
-const NotFoundPage = () => {
+const NotFoundPage = ({ data: { favicon } }) => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <div className="not-found">
+      <SeoDatoCMS favicon={favicon} />
+
+      <div className="container">
+        <h1>Page not found</h1>
+        <p>
+          Sorry 😔, we couldn’t find what you were looking for.
+          <br />
+          <br />
+          <Link className="button-block primary" to="/">
+            Go home
+          </Link>
+        </p>
+      </div>
+    </div>
   );
 };
 
 export default NotFoundPage;
+
+export const NotFoundQuery = graphql`
+  query NotFoundQuery {
+    favicon: datoCmsSite {
+      faviconMetaTags {
+        ...GatsbyDatoCmsFaviconMetaTags
+      }
+    }
+  }
+`;
 
 export const Head = () => <title>Not found</title>;
