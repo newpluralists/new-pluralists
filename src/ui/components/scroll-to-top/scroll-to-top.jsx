9fcbc301ca3dd30efab62839d073888811dd224a
Rodrigo Leon <>
2024-12-17 11:24:29 -0300

    UI Scroll to top component

--- src/ui/components/scroll-to-top/scroll-to-top.jsx
+++ src/ui/components/scroll-to-top/scroll-to-top.jsx
@@ -12,7 +12,16 @@ const ScrollToTop = ({ to }) => {
 
   return (
     <button className="ui-scroll-top" onClick={handleOnScrollTo}>
-      Get back to top
+      Back to top
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+        <path
+          d="M3.75 8.75L10 2.5M10 2.5L16.25 8.75M10 2.5V17.5"
+          stroke="black"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
     </button>
   );
 };
