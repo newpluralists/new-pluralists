f43db90fe20485cde8738407abeeee14b603adb2
Rodrigo Leon <>
2024-12-20 12:26:59 -0300

    qa fix

--- src/ui/blocks/block-narrative-full/block-narrative-full.jsx
+++ src/ui/blocks/block-narrative-full/block-narrative-full.jsx
@@ -8,29 +8,31 @@ const BlockNarrativeFull = ({ block }) => {
   const { title, introduction, mainImage, ctas } = block;
 
   return (
-    <section
-      className="block-narrative-full"
-      style={{
-        backgroundImage: `url(${mainImage.url})`,
-        backgroundPosition: '50dvw',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: '120%',
-      }}
-    >
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-6">
-            <FadeIn>
-              <div className="meta">
-                {title && <div className="title" dangerouslySetInnerHTML={{ __html: title }} />}
-                {introduction && <div className="introduction" dangerouslySetInnerHTML={{ __html: introduction }} />}
-                {ctas && <ButtonList buttons={ctas} />}
-              </div>
-            </FadeIn>
+    <FadeIn>
+      <section
+        className="block-narrative-full"
+        style={{
+          backgroundImage: `url(${mainImage.url})`,
+          backgroundPosition: '50dvw',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: '120%',
+        }}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <FadeIn>
+                <div className="meta">
+                  {title && <div className="title" dangerouslySetInnerHTML={{ __html: title }} />}
+                  {introduction && <div className="introduction" dangerouslySetInnerHTML={{ __html: introduction }} />}
+                  {ctas && <ButtonList buttons={ctas} />}
+                </div>
+              </FadeIn>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </FadeIn>
   );
 };
 
