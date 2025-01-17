0babd3f4135bde7269c171588b416fb835150ba3
Rodrigo Leon <rodrigoleonmdp@gmail.com>
2025-01-17 13:09:16 -0300

    Footer: button to sign up form

--- src/ui/layout/footer/footer-wrapper.jsx
+++ src/ui/layout/footer/footer-wrapper.jsx
@@ -26,6 +26,7 @@ const FooterWrapper = () => {
         }
         formIntroduction
         formLegalText
+        formSignUpUrl
       }
     }
   `);
@@ -69,8 +70,15 @@ const FooterWrapper = () => {
             <div className="form">
               <h4>Stay informed</h4>
               <div className="description" dangerouslySetInnerHTML={{ __html: footer.formIntroduction }} />
-              <MailchimpForm />
-              <div className="legal-text" dangerouslySetInnerHTML={{ __html: footer.formLegalText }} />
+
+              {footer.formSignUpUrl && (
+                <a href={footer.formSignUpUrl} target="_blank" rel="noreferrer" className="button-block white">
+                  Sign up
+                </a>
+              )}
+
+              {/* <MailchimpForm /> */}
+              {/* <div className="legal-text" dangerouslySetInnerHTML={{ __html: footer.formLegalText }} /> */}
             </div>
           </div>
         </div>
@@ -79,53 +87,42 @@ const FooterWrapper = () => {
   );
 };
 
-const MailchimpForm = () => (
-  <div id="mc_embed_shell">
-    <div id="mc_embed_signup">
-      <form
-        action="https://newpluralists.us1.list-manage.com/subscribe/post?u=e47af12b3e9793c2089db0c53&amp;id=84b7d8e357&amp;f_id=00f2f1e5f0"
-        method="post"
-        id="mc-embedded-subscribe-form"
-        name="mc-embedded-subscribe-form"
-        className="validate"
-        target="_self"
-        novalidate=""
-      >
-        <div id="mc_embed_signup_scroll" className="wrapper">
-          <div className="mc-field-group">
-            <input type="text" name="FNAME" className="text" id="mce-FNAME" placeholder="First Name" required />
-          </div>
-          <div className="mc-field-group">
-            <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" placeholder="Email" required />
-          </div>
-          <div hidden>
-            <input type="hidden" name="tags" value="5811177" />
-          </div>
-          <div id="mce-responses" className="clear" style={{ display: 'none' }}>
-            <div className="response" id="mce-error-response" style={{ display: 'none' }}></div>
-            <div className="response" id="mce-success-response" style={{ display: 'none' }}></div>
-          </div>
-          <div aria-hidden="true" style={{ position: 'absolute', left: '-5000px', display: 'none' }}>
-            <input type="text" name="b_e47af12b3e9793c2089db0c53_84b7d8e357" tabIndex="-1" value="" />
-          </div>
-          <div className="clear">
-            <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button" value="Subscribe" />
-          </div>
-        </div>
-      </form>
-    </div>
-  </div>
-);
-
-const AxiosForm = () => {
-  return (
-    <iframe
-      src="https://new-pluralists.read.axioshq.com/signup/new-pluralists-news"
-      width="100%"
-      height="790px"
-      style={{ border: 0 }}
-    />
-  );
-};
+// const MailchimpForm = () => (
+//   <div id="mc_embed_shell">
+//     <div id="mc_embed_signup">
+//       <form
+//         action="https://newpluralists.us1.list-manage.com/subscribe/post?u=e47af12b3e9793c2089db0c53&amp;id=84b7d8e357&amp;f_id=00f2f1e5f0"
+//         method="post"
+//         id="mc-embedded-subscribe-form"
+//         name="mc-embedded-subscribe-form"
+//         className="validate"
+//         target="_self"
+//         novalidate=""
+//       >
+//         <div id="mc_embed_signup_scroll" className="wrapper">
+//           <div className="mc-field-group">
+//             <input type="text" name="FNAME" className="text" id="mce-FNAME" placeholder="First Name" required />
+//           </div>
+//           <div className="mc-field-group">
+//             <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" placeholder="Email" required />
+//           </div>
+//           <div hidden>
+//             <input type="hidden" name="tags" value="5811177" />
+//           </div>
+//           <div id="mce-responses" className="clear" style={{ display: 'none' }}>
+//             <div className="response" id="mce-error-response" style={{ display: 'none' }}></div>
+//             <div className="response" id="mce-success-response" style={{ display: 'none' }}></div>
+//           </div>
+//           <div aria-hidden="true" style={{ position: 'absolute', left: '-5000px', display: 'none' }}>
+//             <input type="text" name="b_e47af12b3e9793c2089db0c53_84b7d8e357" tabIndex="-1" value="" />
+//           </div>
+//           <div className="clear">
+//             <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button" value="Subscribe" />
+//           </div>
+//         </div>
+//       </form>
+//     </div>
+//   </div>
+// );
 
 export default FooterWrapper;
